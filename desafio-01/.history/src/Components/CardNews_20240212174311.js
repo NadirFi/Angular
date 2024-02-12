08aa38f15052b6ede1e8b606a21babfa9f7291cf
NadirFi <nadirssfilho@gmail.com>
2024-02-12 19:15:21 -0300

    Completando Desafio HTLM CSS JS

---
+++ desafio-01/.history/src/Components/CardNews_20240212174311.js
@@ -0,0 +1,32 @@
+class CardNews extends HTMLElement {
+    constructor() {
+        super();
+
+        const shadow = this.attachShadow({ mode: "open"});
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
+    }
+
+    build() {
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
+
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card__left");
+        
+        const cardRight = document.createElement("div");
+        cardLeft.setAttribute("class", "card__right");
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
+    }
+
+    styles() {
+
+    }
+
+}
+
+customElements.define("card-news", CardNews);
---
+++ desafio-01/.history/src/Components/CardNews_20240212180908.js
@@ -0,0 +1,55 @@
+class CardNews extends HTMLElement {
+    constructor() {
+        super();
+
+        const shadow = this.attachShadow({ mode: "open"});
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
+    }
+
+    build() {
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
+
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card__left");
+
+        const autor = document.createElement("span");
+        autor.textContent = "By " + (this.getAttribute("autor") || "Anonymous");
+
+        const linkTitle = document.createElement("a");
+        linkTitle.textContent = this.getAttribute("title");
+        linkTitle.href = this.getAttribute("url-link");
+
+        const newsContent = document.createElement("p");
+        newsContent.textContent = this.getAttribute("contet")
+        
+        cardLeft.appendChild(autor);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
+        
+        const cardRight = document.createElement("div");
+        cardLeft.setAttribute("class", "card__right");
+
+        const newsImage = document.createElement("img");
+        newsImage.src = "assets/Darth__Vader.jpg";
+        newsImage.alt = "Foto da Noticia";
+        newsImage.width = 320;
+        newsImage.height = 205;
+
+        cardRight.appendChild(newsImage);
+
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
+    }
+
+    styles() {
+
+    }
+
+}
+
+customElements.define("card-news", CardNews);
